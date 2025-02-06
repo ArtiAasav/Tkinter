52c5115776598820cae05ff88c2eb6f4d88a23cf
ArtiAasav <arti@hkhk.edu.ee>
2025-02-06 09:59:36 +0200

    .

--- t05.py
+++ t05.py
@@ -17,20 +17,28 @@ def main():
         vastus = tk.Label(aken, text=f"{maksete_arv:.2f}")
         vastus.pack()
 
+    # Loo raamid
+    frame = tk.Frame(aken)
+    frame.pack(pady=5, padx=5, fill="both")
+    frame2 = tk.Frame(aken)
+    frame2.pack(pady=5, padx=5, fill="both")
+    frame3 = tk.Frame(aken)
+    frame3.pack(pady=5, padx=5, fill="both")
+
     # Esimene sisestusväli
-    label = tk.Label(aken, text="Laenusumma (€)").pack()
-    sisestus1 = tk.Entry(aken)
-    sisestus1.pack()
+    label1 = tk.Label(frame, text="Laenusumma (€)").pack(side='left')
+    sisestus1 = tk.Entry(frame)
+    sisestus1.pack(side='left', expand=True, fill="x")
    
     # Teine sisestusväli
-    label = tk.Label(aken, text="Aastane intressimäär (%)").pack()
-    sisestus2 = tk.Entry(aken)
-    sisestus2.pack()
+    label2 = tk.Label(frame2, text="Aastane intressimäär (%)").pack(side='left')
+    sisestus2 = tk.Entry(frame2)
+    sisestus2.pack(side='left', expand=True, fill="x")
 
     # Teine sisestusväli
-    label = tk.Label(aken, text="Laenuperiood (aastad)").pack()
-    sisestus3 = tk.Entry(aken)
-    sisestus3.pack()
+    label3 = tk.Label(frame3, text="Laenuperiood (aastad)").pack(side='left')
+    sisestus3 = tk.Entry(frame3)
+    sisestus3.pack(side='left', expand=True, fill="x")
    
     # Nupp, mis käivitab funktsiooni kuva_sisestus
     nupp = tk.Button(aken, text="Arvuta", command=kuva_sisestus)
